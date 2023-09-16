6b25882049f75b0de99eede5fb11a3310d4daf2a
Dan Hackworth <danielhackworth515@gmail.com>
2023-09-16 02:52:30 -0400

    add stars instead of &

--- home/script.js
+++ home/script.js
@@ -2,4 +2,4 @@ const h2 = document.createElement("h2");
 h2.textContent = "Created by Daniel Hackworth";
 document.querySelector("body").appendChild(h2);
 
-
+//viking text the * is a star # a knot and @ a fancy a
