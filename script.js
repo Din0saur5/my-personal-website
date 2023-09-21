45d0a206ec11be5a1c2159cd4c45e930f89930e4
Dan Hackworth <danielhackworth515@gmail.com>
2023-09-20 21:40:28 -0400

    change design, add better graphics, space theme maintained

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ h2.textContent = "Created by Daniel Hackworth";
 document.querySelector("body").appendChild(h2);
 
 //viking text the * is a star # a knot and @ a fancy a
+document.getElementById('start').addEventListener('click', function () {
+    alert('Button clicked!'); // Display an alert when the button is clicked
+});
+
+function consoleLoadingSequence(){
+    
+}
